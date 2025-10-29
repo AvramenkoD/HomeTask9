d5901c9d784c8dc59d9920bc8c0e54b96ab0eb15
AvramenkoD <dsdautor227@gmail.com>
2025-10-29 20:14:38 +0300

    Добавьте файлы проекта.

---
+++ source.c
@@ -0,0 +1,26 @@
+﻿#include <locale.h>
+#include <stdio.h>
+
+int main()
+{
+	int repeatF, side;
+
+	setlocale(LC_ALL, "RUS");
+
+	do
+	{
+		printf("Введите стору квадрата: ");
+		scanf("%d", &side);
+
+		for (int i = 1; i <= side; i++)
+		{
+			for (int j = 1; j <= side; j++)
+				printf("*");
+			printf("\n");
+		}
+		printf("\nЖелаете повторить 0 - нет, иначе - да: ");
+		scanf("%d", &repeatF);
+	}
+	while (repeatF);
+	system("pause");
+}
