9002f23d034909fa30cde431bd438607b00fe981
AvramenkoD <dsdautor227@gmail.com>
2025-10-29 20:41:11 +0300

    4

--- source.c
+++ source.c
@@ -4,23 +4,27 @@
 int main()
 {
 	int repeatF, side;
+	char symbol;
 
 	setlocale(LC_ALL, "RUS");
+	repeatF = 0;
 
 	do
 	{
-		printf("Введите стору квадрата: ");
-		scanf("%d", &side);
+		repeatF = 0;
+		printf("Введите стору квадрата и символ через пробел: ");
+		scanf("%d %c", &side, &symbol);
 
 		for (int i = 1; i <= side; i++)
 		{
-			for (int j = 1; j <= side; j++)
-				printf("*");
 			printf("\n");
+			for (int j = 1; j <= side; j++)
+				printf("%c",symbol);
 		}
 		printf("\nЖелаете повторить 0 - нет, иначе - да: ");
 		scanf("%d", &repeatF);
 	}
 	while (repeatF);
+	puts("");
 	system("pause");
 }
